3fa5419d428e8097bf9c1f8161cf896632546aa2
Mike-Gag <t8200234@aueb.gr>
2022-12-30 15:43:39 +0200

    estrwsa ligo to codebase ergasteite analogws pls

--- solverr.py
+++ solverr.py
@@ -5,9 +5,24 @@ from VRP_Model import *
 
 
 class Solution:
+
     def __init__(self):
         self.cost = 0.0
         self.routes = []
+    
+    def print_results(self):
+        tot_cost = 0
+        true_total_cost = 0
+        for k in self.routes:
+            for n in k.sequenceOfNodes:
+                print(str(n.ID) + ", ", end="")
+            print()
+            tot_cost+=k.cost
+            rt_cost, load = calculate_route_details(k.sequenceOfNodes)
+            true_total_cost+=rt_cost
+
+        print(tot_cost)
+        print(true_total_cost)
 
 
 class Saving:
@@ -18,6 +33,7 @@ class Saving:
 
 
 class Solver:
+
     def __init__(self, m):
         self.allNodes = m.allNodes
         self.customers = m.customers
@@ -26,59 +42,69 @@ class Solver:
         self.capacity = m.capacity
         self.sol = None
         self.bestSolution = None
+        self.no_of_vehicles = 14
+        self.used = {self.depot.ID}
 
+    #vriskei ton kontinotero komvo sto route r pou exei teleutaio komvo ton n
+    #dinei to index tou komvou kai to dist tou apo ton n
+    def find_node(self,r, n):
+        dist = self.distanceMatrix[n.ID]
+        nearest_v = 10000000
+        nearest_index = 0
+        for i in range(0, len(dist) - 1):
+            if dist[i] < nearest_v and i not in self.used and r.load + self.allNodes[i].demand < r.capacity:
+                nearest_index = i
+                nearest_v = dist[i]
+        self.used.add(nearest_index)
+        return nearest_index, nearest_v
 
-model = Model()
-model.BuildModel()
-solver = Solver(model)
-routes = []
-used = {solver.depot.ID}
-
-
-def find_node(r, n):
-    dist = solver.distanceMatrix[n.ID]
-    nearest_v = 10000000
-    nearest_index = 0
-    for i in range(0, len(dist) - 1):
-        if dist[i] < nearest_v and i not in used and r.load + solver.allNodes[i].demand < r.capacity:
-            nearest_index = i
-            nearest_v = dist[i]
-    used.add(nearest_index)
-    return nearest_index, nearest_v
-
-
-def nearestneighbor():
-    for i in range(0, 14):
-        routes.append(Route(solver.depot, solver.capacity))
-    j = 0
-    for i in range(1, len(solver.allNodes) - 1):
-        route = routes[j % 14]
-        node = route.sequenceOfNodes[-1]
-        nearest_possible, value = find_node(route, node)
-        solver.allNodes[nearest_possible].isRouted = True
-        if(len(route.sequenceOfNodes)==1):
-            solver.allNodes[nearest_possible].waitingtime=value
-            print("here1")
-        else:
-            solver.allNodes[nearest_possible].waitingtime=route.sequenceOfNodes[-1].waitingtime+value
-            print("here2")
-        route.load += solver.allNodes[nearest_possible].demand
-        route.cost += solver.allNodes[nearest_possible].waitingtime
-        solver.allNodes[nearest_possible].waitingtime+=10
-        route.sequenceOfNodes.append(solver.allNodes[nearest_possible])
+    #ftiaxnei arxikh lysh nearest neighbor 
+    #epi tou solution attribute tou solver object (self.sol)
+    def nearestneighbor(self):
+        for i in range(0, self.no_of_vehicles):
+            self.sol.routes.append(Route(self.depot, self.capacity))
+        j = 0
+        for i in range(1, len(self.allNodes) - 1):
+            route = self.sol.routes[j % self.no_of_vehicles]
+            node = route.sequenceOfNodes[-1]
+            nearest_possible, value = self.find_node(route, node)
+            self.allNodes[nearest_possible].isRouted = True
+            if(len(route.sequenceOfNodes)==1):
+                self.allNodes[nearest_possible].waitingtime = value
+                print("here1")
+            else:
+                self.allNodes[nearest_possible].waitingtime=route.sequenceOfNodes[-1].waitingtime + value
+                print("here2")
+            route.load += self.allNodes[nearest_possible].demand
+            route.cost += self.allNodes[nearest_possible].waitingtime
+            self.allNodes[nearest_possible].waitingtime += 10
+            route.sequenceOfNodes.append(self.allNodes[nearest_possible])
 
 
 
-        j += 1
+            j += 1
+    #solve
+    # otan mpoun kai ta operators tha kallountai apo edw oxi sth main 
+    def solve(self):
+            
+        model = Model()
+        model.BuildModel()
+
+        self.sol = Solution()
+        self.nearestneighbor()
+
+        self.sol.print_results()
 
 
+#eykleidia apostash 2 komvwn
 def distance(from_node, to_node):
     dx = from_node.x - to_node.x
     dy = from_node.y - to_node.y
     dist = math.sqrt(dx ** 2 + dy ** 2)
     return dist
 
-
+#dwse ena route
+#pare pisw to cost kai ton forto
 def calculate_route_details(nodes_sequence):
     rt_load = 0
     rt_cumulative_cost = 0
@@ -92,9 +118,7 @@ def calculate_route_details(nodes_sequence):
         rt_load += from_node.demand
     return rt_cumulative_cost, rt_load
 
-nearestneighbor()
-sol = Solution()
-sol.routes = routes
+
 # plot solution
 # for r in range(0, len(sol.routes)):
 #     rt = sol.routes[r]
@@ -104,15 +128,15 @@ sol.routes = routes
 #         plt.plot([c0.x, c1.x], [c0.y, c1.y])
 #
 # plt.show()
-tot_cost =0
-true_total_cost = 0
-for k in routes:
-    for n in k.sequenceOfNodes:
-        print(str(n.ID) + ", ", end="")
-    print()
-    tot_cost+=k.cost
-    rt_cost, load = calculate_route_details(k.sequenceOfNodes)
-    true_total_cost+=rt_cost
 
-print(tot_cost)
-print(true_total_cost)
+#edw einai h main
+#apla ftiaxneis ena solver kai patas .solve() gia na pareis lysh
+#.print_results() gia apotelesmata
+if __name__=="__main__":
+
+    model = Model()
+    model.BuildModel()
+
+    solver = Solver(model)
+    solver.solve()
+    solver.sol.print_results()
